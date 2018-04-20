d25d4e126318fce7dd161fc2ea7e13dd51d00846
mitya <kropotovd@gmail.com>
2018-04-20 11:58:14 +0300

    lab4

---
+++ lab4/src/test/java/lab/model/SimpleAppTest.java
@@ -0,0 +1,60 @@
+package lab.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SimpleAppTest {
+
+	protected static final String APPLICATION_CONTEXT_XML_FILE_NAME = "ioc.xml";
+
+	private AbstractApplicationContext context;
+
+	private UsualPerson expectedPerson;
+
+	@Before
+	public void setUp() throws Exception {
+		context = new ClassPathXmlApplicationContext(
+				APPLICATION_CONTEXT_XML_FILE_NAME);
+		expectedPerson = getExpectedPerson();
+	}
+
+	@Test
+	public void testInitPerson() {
+		UsualPerson person = (UsualPerson) context.getBean("person");
+//		FYI: Another way to achieve the bean
+		UsualPerson person2 = context.getBean(UsualPerson.class);
+		assertEquals(expectedPerson, person);
+		System.out.println(person);
+        System.out.println(person2);
+    }
+
+	private UsualPerson getExpectedPerson() {
+		UsualPerson person = new UsualPerson();
+		person.setAge(35);
+		person.setHeight(1.78F);
+		person.setIsProgrammer(true);
+		person.setName("John Smith");
+
+		Country country = new Country();
+		country.setId(1);
+		country.setName("Russia");
+		country.setCodeName("RU");
+
+		person.setCountry(country);
+
+		List<String> contacts = new ArrayList();
+		contacts.add("asd@asd.ru");
+		contacts.add("+7-234-456-67-89");
+
+		person.setContacts(contacts);
+
+		return person;
+	}
+}
